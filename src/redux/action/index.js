31eaee0568981d8b7edff836f400ada1385cd4ab
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-21 00:19:07 +0500

    commit

---
+++ src/redux/action/index.js
@@ -0,0 +1,15 @@
+
+
+export const addCart = (product) =>{
+    return{
+        type: "ADDITEM",
+        payload : product
+    }
+}
+
+export const delCart = (product) => {
+    return{
+        type: "DELITEM",
+        payload : product
+    }
+}
