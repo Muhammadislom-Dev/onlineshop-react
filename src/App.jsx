71e6ddd5499ae32e98962a56b65bfdfcebe65b24
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-21 00:18:49 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import "aos/dist/aos.css";
 import { useEffect } from 'react';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import {Switch, Route} from 'react-router-dom';
+import Product from './components/Product';
+import Products from './components/Products';
 
 function App() {
   useEffect(() => {
@@ -14,7 +17,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Product} />
+        <Route exact  path="/products/:id" component={Products} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+   <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  document.getElementById("root")
 );
 
